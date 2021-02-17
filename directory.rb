2b24f2f0d07c952552871e687fbe75a8394e4201
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-17 17:20:12 +0000

    Added number of students to directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First, we print out the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Finally, we print the total number of students
+print "Overall, we have "
+# It's important that unlike puts(), print() doesn't add new lines
+print 11
+print " great students"
