557ed89d1cea03f1f480442da17c91338e46ea7d
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-19 12:17:46 +0000

    Created duplicate file for Step 8 exercises

---
+++ directory_exercises.rb
@@ -0,0 +1,35 @@
+def input_student
+  puts "Please enter student name to add."
+  puts "To finish, simply hit return twice."
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :November}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# Nothing happens until we actually call the methods:
+students = input_student
+print_header
+print(students)
+print_footer(students)
