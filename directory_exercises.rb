263355f640b58e59da316babfe1531138172157b
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-19 16:22:29 +0000

    Exercise 7 (changed input_students method to allow user to specify student cohort)

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,51 +1,55 @@
-# def input_student
-#   puts "Please enter student name to add."
-#   puts "To finish, simply hit return twice."
-#   # create an empty array
-#   students = []
-#   # get the first name
-#   name = gets.chomp
-#   # while the name is not empty, repeat this code
-#   while !name.empty? do
-#     # add the student hash to the array
-#     students << {name: name, cohort: :November}
-#     puts "Now we have #{students.count} students"
-#     # get another name from the user
-#     name = gets.chomp
-#   end
-#   # return the array of students
-#   students
-# end
-students = [
-  # 5. Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc.
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobby: "cooking"},
-  {name: "Darth Vader", cohort: :november, hobby: "using the Force"},
-  {name: "Nurse Ratched", cohort: :november, hobby: "caring for others"},
-  {name: "Michael Corleone", cohort: :november, hobby: "making people offers they can't refuse"},
-  {name: "Alex DeLarge", cohort: :november, hobby: "a bit of the old ultra-violence"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobby: "flying"},
-  {name: "Terminator", cohort: :november, hobby: "terminating"},
-  {name: "Freddy Krueger", cohort: :november, hobby: "making new friends"},
-  {name: "The Joker", cohort: :november, hobby: "practical jokes"},
-  {name: "Joffrey Baratheon", cohort: :november, hobby: "being a dick"},
-  {name: "Norman Bates", cohort: :november, hobby: "spending time with family"}
-]
-# 6. Research how the method center() of the String class works. Use it in your code
-# to make the output beautifully aligned.
+# 7. In the input_students method the cohort value is hard-coded. How can you ask for both the name and the cohort?
+# What if one of the values is empty? Can you supply a default value?
+# The input will be given to you as a string. How will you convert it to a symbol?
+# What if the user makes a typo?
+def input_student
+  puts "Please enter student name to add and hit return."
+  puts "Then, enter that student's cohort and hit return."
+  puts "To finish, simply hit return without adding text."
+  # create an empty array
+  students = []
+  # create list of possible cohorts
+  cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  # get cohort
+    cohort = gets.chomp.downcase.to_sym
+    # set default to current cohort in case field is left empty
+    # in a real-world scenario it would make more sense to have a separate varaible to store current cohort
+    if cohort.empty?
+      cohort = :march
+    end
+    # check whether user has input an real cohort (or made a typo/input some other value)
+    while !cohorts.include?(cohort)
+      puts "Invalid cohort entered. Enter a valid month name: "
+      cohort = gets.chomp.downcase.to_sym
+    end
+    # add the student hash to the array
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy".center(200, " ")
   puts "-------------".center(200, " ")
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort), who likes #{student[:hobby]}".center(200, " ")
+    puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)".center(200, " ")
   end
 end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(200, " ")
 end
 # Nothing happens until we actually call the methods:
-# students = input_student
+students = input_student
 print_header
 print(students)
 print_footer(students)
