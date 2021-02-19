1c50f32b231b5ca6d626fbfbabfed89e16aa34fd
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-19 19:45:46 +0000

    Exercise 8 (displaying names grouped by cohorts; also fixed a glitch in input_students - now allowing default cohort even if initial inputs are invalid)

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,7 +1,3 @@
-# 7. In the input_students method the cohort value is hard-coded. How can you ask for both the name and the cohort?
-# What if one of the values is empty? Can you supply a default value?
-# The input will be given to you as a string. How will you convert it to a symbol?
-# What if the user makes a typo?
 def input_student
   puts "Please enter student name to add and hit return."
   puts "Then, enter that student's cohort and hit return."
@@ -16,13 +12,14 @@ def input_student
   while !name.empty? do
   # get cohort
     cohort = gets.chomp.downcase.to_sym
-    # set default to current cohort in case field is left empty
-    # in a real-world scenario it would make more sense to have a separate varaible to store current cohort
-    if cohort.empty?
-      cohort = :march
-    end
     # check whether user has input an real cohort (or made a typo/input some other value)
     while !cohorts.include?(cohort)
+      # set default to current cohort in case field is left empty
+      # in a real-world scenario it would make more sense to have a separate varaible to store current cohort
+      if cohort.empty?
+        cohort = :march
+        break
+      end
       puts "Invalid cohort entered. Enter a valid month name: "
       cohort = gets.chomp.downcase.to_sym
     end
@@ -37,21 +34,39 @@ def input_student
 end
 
 def print_header
-  puts "The students of Villains Academy".center(200, " ")
-  puts "-------------".center(200, " ")
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-def print(students)
+# 8. Once you complete the previous exercise, change the way the users are displayed: print them grouped by cohorts.
+# To do this, you'll need to get a list of all existing cohorts (the map() method may be useful but it's not the only option),
+# iterate over it and only print the students from that cohort.
+
+# My note: This code works; however, it prints out the cohorts based on the order in which they were entered.
+# In the future it would be interesting to get it to rearrange the cohorts based on calendar order.
+def sort_students(students)
+  # students_by_cohort = students.map { |student| }
+  students_by_cohort = {}
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)".center(200, " ")
+    cohort = student[:cohort]
+    if students_by_cohort[cohort] == nil
+      students_by_cohort[cohort] = []
+    end
+    students_by_cohort[cohort].push(student[:name])
+  end
+  return students_by_cohort
+end
+def print(students_by_cohort)
+  students_by_cohort.each do |cohort, names|
+    puts "#{cohort.capitalize} cohort:"
+    names.each {|name| puts name}
   end
 end
-# Right now if we have only one student, the user will see a message "Now we have 1 students", whereas it should be
-# "Now we have 1 student". How can you fix it so that it used singular form when appropriate and plural form otherwise?
 def print_footer(names)
-  puts  "Overall, we have #{names.count} great #{names.count == 1 ? "student" : "students"}".center(200, " ")
+  puts  "Overall, we have #{names.count} great #{names.count == 1 ? "student" : "students"}"
 end
 # Nothing happens until we actually call the methods:
 students = input_student
+students_by_cohort = sort_students(students)
 print_header
-print(students)
+print(students_by_cohort)
 print_footer(students)
