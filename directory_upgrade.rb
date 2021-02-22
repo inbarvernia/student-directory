ae34d5839bf79d52371d48b1d899ac223aab027e
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-22 18:43:21 +0000

    Added option for loading list from students.csv

--- directory_upgrade.rb
+++ directory_upgrade.rb
@@ -52,6 +52,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -63,17 +72,8 @@ def print_menu
   puts "1. Input students"
   puts "2. Show list of students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
-end
-
-def show_students
-  if !@students.empty?
-    print_header
-    print_student_list
-    print_footer
-  else
-    puts "There are currently no students enrolled."
-  end
 end
 
 def process(selection)
@@ -84,6 +84,8 @@ def process(selection)
       show_students
     when 3
       save_students
+    when 4
+      load_students
     when 9
       exit # this will cause the program to terminate
     else
@@ -104,6 +106,16 @@ end
 
 def print_footer
   puts  "Overall, we have #{@students.count} great #{@students.count == 1 ? "student" : "students"}"
+end
+
+def show_students
+  if !@students.empty?
+    print_header
+    print_student_list
+    print_footer
+  else
+    puts "There are currently no students enrolled."
+  end
 end
 
 interactive_menu
